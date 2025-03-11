12b7d6bf9da11cf50a6809cc21f6be5f2b3147c4
Charles Owuala <51397595+CHARLE123455@users.noreply.github.com>
2025-03-11 14:55:23 +0100

    Removed node_modules and .env from Git tracking

--- node_modules/objection/lib/model/getModel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// A small helper method for cached lazy-importing of the Model class.
-let Model;
-const getModel = () => Model || (Model = require('./Model').Model);
-
-module.exports = {
-  getModel,
-};
